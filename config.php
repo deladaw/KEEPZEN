8f0a9b1427e141f64225d90624c8c5a9d0f9dd0c
Daniela Bru <74151869+deladaw@users.noreply.github.com>
2023-06-12 13:40:18 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,7 @@ require_once('vendor/autoload.php');
 
 
 $stripe = array(
-    'secret_key' => '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',
-    'publishable_key' => 'pk_live_51NHSo3EEojTiB1j2He80ZQBC61LQFhV41zm6jYzmTb1gk6qTBKu5ROKI6HdMI1xueyYD75Wt6o7Q8GeCsAK4H0Ss00ApnOam1b',
+    
 );
 
 
